ed6ba051e56e70cba677a9ba229eef247cf6ef3d
Walid2907 <kerdadwalid0@gmail.com>
2025-11-16 21:23:26 +0100

    the stucture of code

---
+++ get_next_line_utils.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wkerdad <wkerdad@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 19:04:45 by wkerdad           #+#    #+#             */
+/*   Updated: 2025/11/16 20:55:36 by wkerdad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"get_next_line.h"
+
+void    ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (s == NULL)
+		return ;
+	while (*s != '\0')
+	{
+		write(fd, &*s, 1);
+		s++;
+	}
+}
